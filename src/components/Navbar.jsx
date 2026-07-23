0a80b93cb8ec37010a4781b2680f51c53e377d3a
Subashketagoda <100581926+Subashketagoda@users.noreply.github.com>
2026-07-24 01:52:21 +0530

    Fix navbar logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-import logo from '../assets/mascot.png';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
